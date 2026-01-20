6169856ab70e483c33aea3d9163dce8c72cf1d52
Aritmeos Sanchez Miranda <sancheez@shernet.sheridancollege.ca>
2026-01-20 10:55:57 -0500

    COmpleted Sprite Changer

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,25 +8,33 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public List<Sprite> barrels;
+    public int randomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
          //PickARandomColor();
+         PickARandomSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Keyboard.current.anyKey.wasPressedThisFrame)
-        //{
-        //    PickARandomColor();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            Debug.Log("Try to change the sprite please");
+            //PickARandomColor();
+            if (barrels.Count > 0)
+            {
+                PickARandomSprite();
+            }
+        }
 
         // NOT THIS: spriteRenderer.sprite.bounds.Contains()
 
 
         //get mouse position
-        Vector2 mousePos = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         // is it over the shape
         if(spriteRenderer.bounds.Contains(mousePos) == true)
         {
@@ -36,10 +47,25 @@ public class SpriteChanger : MonoBehaviour
             spriteRenderer.color = Color.white;
         }
 
+        if(Mouse.current.leftButton.wasPressedThisFrame == true && barrels.Count > 0)
+        {
+            barrels.RemoveAt(0);
+        }
     }
 
     void PickARandomColor ()
     {
         spriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandomSprite()
+    {
+        //spriteRenderer.sprite = mySprite;
+
+        //pick a random number
+        randomNumber = Random.Range(0, barrels.Count);
+        //use that number to chose a sprite
+        //assign that sprite to the sprite renderer
+        spriteRenderer.sprite = barrels[randomNumber];
     }
 }
